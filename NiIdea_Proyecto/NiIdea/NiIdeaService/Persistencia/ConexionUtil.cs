f87cf685ee2e3c87ae85935523833375bcb42ce7
renzodc <renzo-dc@hotmail.com>
2015-05-21 23:17:47 -0500

    Commit inicial del proyecto

---
+++ NiIdea_Proyecto/NiIdea/NiIdeaService/Persistencia/ConexionUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NiIdeaService.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "Data Source=(local);Initial Catalog=ProyectoControlAmbiental;Integrated Security=SSPI;";
+        }
+    }
+}
