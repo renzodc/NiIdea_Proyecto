5bfd491c22cd3492b69bc4d64fdf33138e2873a9
Glenn19 <glenngarciaperez@gmail.com>
2015-05-30 14:06:59 -0500

    RRHHServiceTest
    Test de pruebas unitarias para la entidad Responsable

---
+++ NiIdea_Proyecto/NiIdea/RRHHServiceTest/ResponsableTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RRHHServiceTest
+{
+    [TestClass]
+    public class ResponsableTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //LLamando al web service
+            RRHHServiceWS.RRHHServiceClient proxy = new RRHHServiceWS.RRHHServiceClient();
+
+            //Obtener
+            RRHHServiceWS.Responsable responsable = proxy.ObtenerResponsable(2);
+            Assert.AreEqual("glenn", responsable.Nombre);
+
+            
+        }
+    }
+}
