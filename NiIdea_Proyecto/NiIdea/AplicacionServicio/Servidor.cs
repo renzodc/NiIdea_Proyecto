4138730edb8732729fb105be90442418b1608399
Glenn19 <glenngarciaperez@gmail.com>
2015-06-14 09:45:50 -0500

    Aplicación de Mensajería para los Clientes
    Creación de Colas para que los clientes lleven un control de estado de
    su Proyecto.

---
+++ NiIdea_Proyecto/NiIdea/AplicacionServicio/Servidor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionServicio
+{
+  public  class Servidor
+    {
+     public   static void Main(string[] args)
+        {
+
+
+            //Recibe la Nota 
+            string rutaCola = @".\private$\cola";
+
+            if (!MessageQueue.Exists(rutaCola))
+                MessageQueue.Create(rutaCola);
+
+            MessageQueue cola = new MessageQueue(rutaCola);
+            cola.Formatter = new XmlMessageFormatter(new Type[] { typeof(MonitoreoProyecto) });
+            Message mensaje = cola.Receive();
+            MonitoreoProyecto monitoreo = (MonitoreoProyecto)mensaje.Body;
+            Console.WriteLine("Asunto: " + mensaje.Label);
+            Console.WriteLine("Cliente: " + monitoreo.Cliente + "Proyecto: " + monitoreo.Proyecto);
+           Console.ReadLine();
+
+            
+
+
+        }
+
+     public class MonitoreoProyecto
+     {
+         public String Cliente { get; set; }
+         public String Proyecto { get; set; }
+     }
+    }
+}
---
+++ NiIdea_Proyecto/NiIdea/SeguimientoMonitoreoCliente/MonitoreoCliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionCliente
+{
+  public  class MonitoreoCliente
+    {
+      public  static void Main(string[] args)
+        {
+            string rutaCola = @".\private$\cola";
+            if (!MessageQueue.Exists(rutaCola))
+                MessageQueue.Create(rutaCola);
+
+            MessageQueue cola = new MessageQueue(rutaCola);
+
+            if (cola.GetAllMessages().Count() == 0)
+            {
+                Message mensaje = new Message();
+                mensaje.Label = "Nuevo Monitoreo Poryecto";
+                mensaje.Body = new MonitoreoProyecto() { Cliente = "Glenn", Proyecto = "Proyecto A&SO" };
+                cola.Send(mensaje);
+                Console.ReadLine();
+
+            }
+        }
+
+        public class MonitoreoProyecto
+        {
+            public String Cliente { get; set; }
+            public String Proyecto { get; set; }
+        }
+    }
+}
